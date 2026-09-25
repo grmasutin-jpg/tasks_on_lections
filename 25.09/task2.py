2b3443004e4f73c61b24775c18d317266210e087
grmasutin-jpg <gr.masutin@yandex.ru>
2026-09-25 16:53:36 +0500

    Create task2.py

---
+++ 25.09/task2.py
@@ -0,0 +1,15 @@
+number = 20
+if number % 2 == 0:
+  print(f"{number} is even")
+else:
+  print(f"{number} is not even")
+if number > 0:
+  print(f"{number} is positive")
+elif number < 0:
+  print(f"{number} is negative")
+else:
+  print("it is 0")
+if 10 <= number <= 50:
+  print(f"{number} is between 10 and 50")
+else:
+  print(f"{number} is not from [10, 50] diapozone")
