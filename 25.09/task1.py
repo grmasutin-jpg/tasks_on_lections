bd088794702784aeb7ee8d387defdba49c53324c
grmasutin-jpg <gr.masutin@yandex.ru>
2026-09-25 16:35:22 +0500

    Create task1.py

---
+++ 25.09/task1.py
@@ -0,0 +1,5 @@
+celcium_deg = float(input())
+kelvin_deg = celcium_deg + 273.15
+farenheight_deg = 9/5 * celcium_deg + 32
+print(f"{celcium_deg}*C = {farenheight_deg}*F")
+print(f"{celcium_deg}*C = {Kelvin_deg}*K"
