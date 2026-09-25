4ecd851111ed1f7ccfc605eb2e0c37a3841a1e9a
grmasutin-jpg <gr.masutin@yandex.ru>
2026-09-25 16:37:24 +0500

    Update task1.py

--- 25.09/task1.py
+++ 25.09/task1.py
@@ -1,5 +1,5 @@
 celcium_deg = float(input())
 kelvin_deg = celcium_deg + 273.15
-farenheight_deg = 9/5 * celcium_deg + 32
+farenheight_deg = 9.0/5.0 * celcium_deg + 32
 print(f"{celcium_deg}*C = {farenheight_deg}*F")
-print(f"{celcium_deg}*C = {Kelvin_deg}*K"
+print(f"{celcium_deg}*C = {kelvin_deg}*K")
